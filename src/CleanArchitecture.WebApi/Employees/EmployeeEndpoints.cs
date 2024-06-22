ebc5ebebee6809086a7a76f36f2857ef67c887a9
Bruno RIbeiro <brunovdribeiro@gmail.com>
2024-06-22 22:16:16 +0100

    Add employee pagination and creation endpoints
    Endpoints for retrieving employees with pagination and creating new employees have been implemented in EmployeeEndpoints.cs. The "get" and "post" methods have been updated to handle these new features. Also, the launchUrl has been moved in the launchSettings.json file for better organization and clarity.

--- src/CleanArchitecture.WebApi/Employees/EmployeeEndpoints.cs
+++ src/CleanArchitecture.WebApi/Employees/EmployeeEndpoints.cs
@@ -1,3 +1,11 @@
+using CleanArchitecture.Application.Employees.Commands.CreateEmployee;
+using CleanArchitecture.Application.Employees.Queries;
+using CleanArchitecture.Contract.Employees;
+
+using MediatR;
+
+using Microsoft.AspNetCore.Mvc;
+
 namespace CleanArchitecture.WebApi.Employees;
 
 public static class EmployeeEndpoints
@@ -6,7 +14,24 @@ public static class EmployeeEndpoints
     {
         var endpoint = app.MapGroup("api/employees/");
 
-        endpoint.MapGet("/", () => "Oi");
+        endpoint.MapGet("{pageNumber:int}/{pageSize:int}", async (
+            ISender mediator, 
+            int pageNumber, 
+            int pageSize) =>
+        {
+            GetEmployeesWithPagination query = new GetEmployeesWithPagination(pageNumber, pageSize);
+
+            return await mediator.Send(query);
+        });
+
+        endpoint.MapPost("/", async (
+            ISender mediator, 
+            CreateEmployeeRequest request) =>
+        {
+            CreateEmployeeCommand command = new CreateEmployeeCommand(request.Name, request.Email);
+        
+            return await mediator.Send(command);
+        });
 
         return app;
     }
